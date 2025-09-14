33f6792286baf0d14f4b6abc131fdac93643aa47
Mrbysco <Mrbysco@users.noreply.github.com>
2025-09-14 22:56:44 +0200

    Port to 1.21.5

--- src/main/java/com/mrbysco/stickerframes/client/renderer/StickerFrameRenderer.java
+++ src/main/java/com/mrbysco/stickerframes/client/renderer/StickerFrameRenderer.java
@@ -9,18 +9,19 @@ import net.minecraft.client.renderer.MapRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
+import net.minecraft.client.renderer.block.ModelBlockRenderer;
+import net.minecraft.client.renderer.block.model.BlockStateModel;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.item.ItemModelResolver;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.renderer.texture.TextureAtlas;
-import net.minecraft.client.resources.model.BlockStateModelLoader;
-import net.minecraft.client.resources.model.ModelManager;
-import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.client.resources.model.BlockStateDefinitions;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.saveddata.maps.MapId;
 import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
 import net.minecraft.world.phys.Vec3;
@@ -63,23 +64,20 @@ public class StickerFrameRenderer<T extends StickerFrame> extends EntityRenderer
 		poseStack.mulPose(Axis.XP.rotationDegrees(f));
 		poseStack.mulPose(Axis.YP.rotationDegrees(f1));
 		if (!renderState.isInvisible) {
-			ModelManager modelmanager = this.blockRenderer.getBlockModelShaper().getModelManager();
-			ModelResourceLocation modelresourcelocation = getFrameModelResourceLocation(renderState);
+			BlockState blockstate = BlockStateDefinitions.getItemFrameFakeState(renderState.isGlowFrame, renderState.mapId != null);
+			BlockStateModel blockstatemodel = this.blockRenderer.getBlockModel(blockstate);
 			poseStack.pushPose();
 			poseStack.translate(-0.5F, -0.5F, -0.5F);
-			this.blockRenderer
-					.getModelRenderer()
-					.renderModel(
-							poseStack.last(),
-							bufferSource.getBuffer(RenderType.entitySolidZOffsetForward(TextureAtlas.LOCATION_BLOCKS)),
-							null,
-							modelmanager.getModel(modelresourcelocation),
-							1.0F,
-							1.0F,
-							1.0F,
-							packedLight,
-							OverlayTexture.NO_OVERLAY
-					);
+			ModelBlockRenderer.renderModel(
+					poseStack.last(),
+					bufferSource.getBuffer(RenderType.entitySolidZOffsetForward(TextureAtlas.LOCATION_BLOCKS)),
+					blockstatemodel,
+					1.0F,
+					1.0F,
+					1.0F,
+					packedLight,
+					OverlayTexture.NO_OVERLAY
+			);
 			poseStack.popPose();
 		}
 
@@ -96,16 +94,16 @@ public class StickerFrameRenderer<T extends StickerFrame> extends EntityRenderer
 			poseStack.scale(0.0078125F, 0.0078125F, 0.0078125F);
 			poseStack.translate(-64.0F, -64.0F, 0.0F);
 			poseStack.translate(0.0F, 0.0F, -1.0F);
-			int i = this.getLightCoords(renderState.isGlowFrame, 15728850, packedLight);
-			this.mapRenderer.render(renderState.mapRenderState, poseStack, bufferSource, true, i);
+			int lightCoords = this.getLightCoords(renderState.isGlowFrame, 15728850, packedLight);
+			this.mapRenderer.render(renderState.mapRenderState, poseStack, bufferSource, true, lightCoords);
 		} else if (!renderState.item.isEmpty()) {
 			poseStack.mulPose(Axis.ZP.rotationDegrees((float) renderState.rotation * 360.0F / 8.0F));
-			int k = this.getLightCoords(renderState.isGlowFrame, 15728880, packedLight);
+			int lightCoords = this.getLightCoords(renderState.isGlowFrame, 15728880, packedLight);
 			poseStack.scale(0.5F, 0.5F, 0.001F);
-			if (renderState.item.isGui3d()) {
+			if (renderState.displayContext == ItemDisplayContext.GUI) {
 				poseStack.mulPose(Axis.YP.rotationDegrees(180.0F));
 			}
-			renderState.item.render(poseStack, bufferSource, k, OverlayTexture.NO_OVERLAY);
+			renderState.item.render(poseStack, bufferSource, lightCoords, OverlayTexture.NO_OVERLAY);
 		}
 
 		poseStack.popPose();
@@ -113,14 +111,6 @@ public class StickerFrameRenderer<T extends StickerFrame> extends EntityRenderer
 
 	private int getLightCoords(boolean isGlowFrame, int glowLight, int normalLight) {
 		return isGlowFrame ? glowLight : normalLight;
-	}
-
-	private static ModelResourceLocation getFrameModelResourceLocation(StickerFrameRenderState renderState) {
-		if (renderState.mapId != null) {
-			return renderState.isGlowFrame ? BlockStateModelLoader.GLOW_MAP_FRAME_LOCATION : BlockStateModelLoader.MAP_FRAME_LOCATION;
-		} else {
-			return renderState.isGlowFrame ? BlockStateModelLoader.GLOW_FRAME_LOCATION : BlockStateModelLoader.FRAME_LOCATION;
-		}
 	}
 
 	@Override
@@ -143,7 +133,8 @@ public class StickerFrameRenderer<T extends StickerFrame> extends EntityRenderer
 		super.extractRenderState(frame, renderState, partialTick);
 		renderState.direction = frame.getDirection();
 		ItemStack itemstack = frame.getItem();
-		this.itemModelResolver.updateForNonLiving(renderState.item, itemstack, frame.usesGuiDisplay() ? ItemDisplayContext.GUI : ItemDisplayContext.FIXED, frame);
+		renderState.displayContext = frame.usesGuiDisplay() ? ItemDisplayContext.GUI : ItemDisplayContext.FIXED;
+		this.itemModelResolver.updateForNonLiving(renderState.item, itemstack, renderState.displayContext, frame);
 		renderState.rotation = frame.getRotation();
 		renderState.isGlowFrame = frame.isGlowing();
 		renderState.mapId = null;
--- src/main/java/com/mrbysco/stickerframes/client/state/StickerFrameRenderState.java
+++ src/main/java/com/mrbysco/stickerframes/client/state/StickerFrameRenderState.java
@@ -1,6 +1,8 @@
 package com.mrbysco.stickerframes.client.state;
 
 import net.minecraft.client.renderer.entity.state.ItemFrameRenderState;
+import net.minecraft.world.item.ItemDisplayContext;
 
 public class StickerFrameRenderState extends ItemFrameRenderState {
+	public ItemDisplayContext displayContext;
 }
